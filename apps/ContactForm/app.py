bbfd8e5907c7b16b6606d24450b690d6a516c1b3
AD\someya_shingo <sekai_love.me.do@hotmail.co.jp>
2022-05-30 18:01:05 +0900

    メール送信実装

--- apps/ContactForm/app.py
+++ apps/ContactForm/app.py
@@ -1,9 +1,11 @@
 import logging
+import os
 
 from email_validator import EmailNotValidError, validate_email
 from flask import (Flask, current_app, flash, g, redirect, render_template,
                    request, url_for)
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.debug = True
@@ -13,11 +15,14 @@ app.logger.setLevel(logging.DEBUG)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 toolbar = DebugToolbarExtension(app)
 
-ctx = app.app_context()
-ctx.push()
-print(current_app.name)
-g.connection = "connection"
-print(g.connection)
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -58,10 +63,24 @@ def contact_complete():
         if not is_valid:
             return redirect(url_for("contact"))
 
+        print("-----checked form-----")
+
         flash("お問い合わせありがとうございました")
-        return redirect(url_for("contact_complete"))
         
+        send_email(
+            email,
+            "お問い合わせありがとうございました。",
+            "contact_mail",
+            username=username,
+            desription=desription,
+        )
+
+        return redirect(url_for("contact_complete"))
     return render_template("contact_complete.html")
 
-with app.test_request_context("/users?updated=true"):
-    print(request.args.get("updated"))
+def send_email(to, subject, template, **kwaargs):
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwaargs)
+    msg.html = render_template(template + ".html", **kwaargs)
+    mail.send(msg)
+    print("-----success send mail-----")
