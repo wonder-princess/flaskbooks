95bbdab8abae9b77de6caccb3ecdd8ea4089a5cf
AD\someya_shingo <sekai_love.me.do@hotmail.co.jp>
2022-06-03 14:25:27 +0900

    configファイル実装

--- apps/app.py
+++ apps/app.py
@@ -1,24 +1,29 @@
-from pathlib import Path
+# from pathlib import Path
 
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
+from apps.config import config
+
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
 
+    app.config.from_object(config[config_key])
+    
+    '''
     app.config.from_mapping(
-        SECRET_KEY="pass8888",
-        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        # SECRET_KEY="pass8888",
+        # SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        # SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="pass8888",
+        # WTF_CSRF_SECRET_KEY="pass8888",
     )
-
+    '''
     csrf.init_app(app)
     db.init_app(app)
     Migrate(app, db)
---
+++ apps/config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+basedir = Path(__name__).parent.parent
+
+class BaseConfig:
+    SECRET_KEY = "pass8888"
+    WTF_CSRF_SECRET_KEY="pass8888"
+
+class LocalConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = True
+
+class TestingConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'test.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+
+config = {
+    "testing": TestingConfig,
+    "local": LocalConfig,
+}
