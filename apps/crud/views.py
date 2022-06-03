9361b53667432fc0742539bcacf1119676d6ccaa
AD\someya_shingo <sekai_love.me.do@hotmail.co.jp>
2022-06-03 13:45:44 +0900

    crudフォーム作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -14,10 +14,10 @@ crud = Blueprint(
 def index():
     return render_template("crud/index.html")
 
-@crud.route("/sql")
-def sql():
-    db.session.query(User).all()
-    return "コンソールログを確認してください"
+@crud.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
 
 @crud.route("/insert")
 def insert():
@@ -31,7 +31,7 @@ def insert():
     return "1件追加しました"
 
 
-@crud.route("/user/new", methods=["GET", "POST"])
+@crud.route("/users/new", methods=["GET", "POST"])
 def create_user():
     form = UserForm()
     if form.validate_on_submit():
@@ -46,9 +46,29 @@ def create_user():
         return redirect(url_for("crud.users"))
     return render_template("crud/create.html", form=form)
 
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+    user = User.query.filter_by(id=user_id).first()
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+
+    return render_template("crud/edit.html", user=user, form=form)
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
+
+
 '''
-@crud.route("/")
-@crud.route("/")
 @crud.route("/")
 '''
 
