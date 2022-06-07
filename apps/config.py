5eab27c2b3f91c600c875072ee64aa82d4f90b3b
AD\someya_shingo <sekai_love.me.do@hotmail.co.jp>
2022-06-07 18:26:02 +0900

    画像アップロード実装

--- apps/config.py
+++ apps/config.py
@@ -5,6 +5,7 @@ basedir = Path(__name__).parent.parent
 class BaseConfig:
     SECRET_KEY = "pass8888"
     WTF_CSRF_SECRET_KEY="pass8888"
+    UPLOAD_FOLDER = str(Path(basedir, "apps", "images"))
 
 class LocalConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}"
--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,7 +1,13 @@
+import uuid
+from pathlib import Path
+
 from apps.app import db
 from apps.crud.models import User
+from apps.detector.forms import UploadImageForm
 from apps.detector.models import UserImage
-from flask import Blueprint, render_template
+from flask import (Blueprint, current_app, redirect, render_template,
+                   send_from_directory, url_for)
+from flask_login import current_user, login_required
 
 dt = Blueprint(
     "detector",
@@ -15,3 +21,31 @@ def index():
     user_images = (db.session.query(User, UserImage).join(UserImage).filter(User.id == UserImage.user_id).all())
 
     return render_template("detector/index.html", user_images=user_images)
+
+@dt.route("/images<path:filename>")
+def image_file(filename):
+    return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
+
+@dt.route("/upload", methods=["GET", "POST"])
+@login_required
+def upload_image():
+    form = UploadImageForm()
+    if form.validate_on_submit():
+        file = form.image.data
+        ext = Path(file.filename).suffix
+        image_uuid_file_name = str(uuid.uuid4()) + ext
+
+        image_path = Path(
+            current_app.config["UPLOAD_FOLDER"], image_uuid_file_name
+        )
+        file.save(image_path)
+
+        user_image = UserImage(
+            user_id=current_user.id, image_path=image_uuid_file_name
+        )
+        db.session.add(user_image)
+        db.session.commit()
+
+        return redirect(url_for("detector.index"))
+    return render_template("detector/upload.html", form=form)
+
